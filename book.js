37ed196ce93adc6a15738ef4a8f8f9f3a1e277b8
sawdahoque234 <sawdahoque6066@gmail.com>
2021-09-02 15:51:53 +0600

    book

---
+++ book.js
@@ -0,0 +1,47 @@
+const error = document.getElementById('error')
+const inputbtn = () => {
+    const inputField = document.getElementById('inputField')
+    const inputText = inputField.value;
+    inputField.value = '';
+    const url = `http://openlibrary.org/search.json?q=${inputText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+        
+            if (displayBook(data.docs)) {
+                error.innerText = "";
+            }
+             else if (inputText === '') {
+                error.innerText = "Search field cannot empty!!!";
+            }
+            else {
+                error.innerText = "No Result Found!";
+            }
+        })
+    const displayBook = data => {
+        const searchBook = document.getElementById('book-details');
+        searchBook.innerHTML = "";
+        data.forEach(book => {
+            const div = document.createElement('div')
+            div.classList.add('book')
+
+            const imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+            console.log(imgUrl)
+            div.innerHTML = `
+            <div  class="col">
+            <div  class="card"> 
+            <img src="${imgUrl}" class="img-fluid mx-auto d-block w-100 h-100">
+            <div class="card-body">
+            <h5 class="card-title text-danger">${book.title}</h5>
+
+            <p class="card-text  text-success">Author:${book.author_name[0]}</p>
+            <p class="card-text">First Publish Year:${book.first_publish_year}</p>
+            </div>
+            </div>
+            </div>
+        `
+            searchBook.appendChild(div);
+        }
+        )
+    }
+}
