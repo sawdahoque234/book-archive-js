62cfc76d507e3117e9d8237a4f8de71b6be9542d
sawdahoque234 <sawdahoque6066@gmail.com>
2021-09-02 19:14:13 +0600

    bookarchive

--- book.js
+++ book.js
@@ -18,7 +18,8 @@ const inputbtn = () => {
                 error.innerText = "No Result Found!";
             }
         })
-    const displayBook = data => {
+        const displayBook = data => {
+        document.getElementById('resultfield').innerHTML=`<h4  class="text-center">Total result: ${data.length}`
         const searchBook = document.getElementById('book-details');
         searchBook.innerHTML = "";
         data.forEach(book => {
@@ -26,15 +27,16 @@ const inputbtn = () => {
             div.classList.add('book')
 
             const imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
-            console.log(imgUrl)
+            // console.log(imgUrl)
             div.innerHTML = `
             <div  class="col">
             <div  class="card"> 
             <img src="${imgUrl}" class="img-fluid mx-auto d-block w-100 h-100">
             <div class="card-body">
             <h5 class="card-title text-danger">${book.title}</h5>
-
-            <p class="card-text  text-success">Author:${book.author_name[0]}</p>
+            <h5 class="card-title text-danger">${book.numfound}</h5>
+            <p class="card-text  text-success">Author_Name:${book.author_name[0]}</p>
+            <small class="card-text">Publisher_Name: ${book.publisher[0]}</small>
             <p class="card-text">First Publish Year:${book.first_publish_year}</p>
             </div>
             </div>
